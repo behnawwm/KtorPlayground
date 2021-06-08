329473479ffba32d5b5e5b6eafcb1c1fe7040cca
behnawwm <behban20@gmail.com>
2021-06-08 12:42:15 +0430

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
+
